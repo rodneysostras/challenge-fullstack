f94e8c4de919a3b127c7bb8aa5f7390ccfa8ecc1
Rodney Sostras <rodney.sostras@gmail.com>
2022-04-09 00:28:08 -0300

    back-end resource base

--- backend/modulo/models.py
+++ backend/modulo/models.py
@@ -2,8 +2,8 @@ import uuid
 from django.db import models
 
 class ModuloModel(models.Model):
-    id = models.IntegerField(unique=True, auto_created=True, editable=False)
-    modulo_uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.AutoField(primary_key=True, editable=False)
+    modulo_uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
     nome = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
 
