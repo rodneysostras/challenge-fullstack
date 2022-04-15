45e5328066e3ddab7ac576f52b6a7b70ac41ea9a
Rodney Sostras <rodney.sostras@gmail.com>
2022-04-14 23:45:06 -0300

    REFACTOR: multiple values ​​possibility

--- frontend/src/components/alert/index.jsx
+++ frontend/src/components/alert/index.jsx
@@ -1,18 +1,28 @@
 import './styles.scss'
 
 export default function Alert(props) {
-    const { value, success, error } = props
-    let typeStyle = success ? ' success' : ''
-    typeStyle += error ? ' error' : ''
+    const { error, value, success } = props
+    var alertColor = success && 'success' || error && 'error'
 
-    return Object.keys(value).map((key) => (
-        <div className={`alert ${typeStyle}`} key={key}>
-            <span>{key}</span>
-            <ol>
-                { value[key] && value[key].map((value, idx) => (
-                    <li key={idx}>{ value }</li>
-                )) }
-            </ol>
-        </div>
-    ))
+    const box = (title, value) => {
+        const list = (value) => (<li>{ value }</li>)
+
+        return (
+            <div className={ `alert ${alertColor || ''}` }>
+                <span>{title}</span>
+                <ol>
+                    { typeof value === 'string' &&  list(value) }
+                    { Array.isArray(value) && value.map((v) => list(v) ) }
+                </ol>
+            </div>
+        )
+    }
+
+    return (
+        <>
+            { Array.isArray(value) && value.map((v) => box("", v)) }
+            { typeof value === 'string' && box("", value) }
+            { typeof value === 'object' && Object.keys(value).map((k) => box(k, value[k])) }
+        </>
+    )
 }
