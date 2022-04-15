015259913cb536bdb3c867598228aff8dc1388ce
Rodney Sostras <rodney.sostras@gmail.com>
2022-04-14 23:53:06 -0300

    FEAT: navigation links

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -1,41 +1,19 @@
-import React from 'react'
-import Card from '../../components/card'
-
-import ModuloService from '../../services/modulo-service'
-
-import ImgBook from '../../assets/img/book.svg'
+import { Link } from 'react-router-dom'
+import LayoutPage from '../../components/layout/page'
 
 import './styles.scss'
 
 export default function HomePage() {
-    const [modulos, setModulos] = React.useState([])
-
-    React.useEffect(() => {
-        ModuloService.list()
-            .then((data) => setModulos(data))
-    }, [])
-
-    const detailCard = (text) => {
-        return (<p>Quantidade de aulas: {text}</p>)
-    }
+    const navigation = []
 
     return (
-            <section className="page__home">
-                <ul>
-                    { modulos.map((mod, idx) => (
-                        <li key={idx}>
-                            <Card
-                                icon={ImgBook}
-                                title={mod.nome}
-                                detail={detailCard(mod.qtd_aulas)}
-                                actionTitle="Veja as aulas"
-                                url={`/modulo/${mod.modulo_id}`}
-                            />
-                        </li>
-                    ))}
-
-                    {modulos.length == 0 && (<span>Sem registros</span>) }
-                </ul>
-            </section>
+        <LayoutPage
+            title="Página Inicial"
+            breadcrumbs={navigation}
+        >
+            <Link to="/modulo">➤ Página Modulo</Link>
+            <br />
+            <Link to="/aula">➤ Página Aula</Link>
+        </LayoutPage>
     )
 }
